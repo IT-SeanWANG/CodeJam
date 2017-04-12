2b2e6f63e863208f0ed4cd897551b85308f4153f
IT-SeanWANG <binexplorer@126.com>
2017-04-12 16:09:20 +0800

    http://www.cplusplus.com/reference/regex/ECMAScript/

--- 2017_1st/Refer1_Q1.cpp
+++ 2017_1st/Refer1_Q1.cpp
@@ -10,7 +10,8 @@ int main(){
     int avg = 0, left = 0;
     cin>>num;
     cin>>str;
-    //www.ideone.com(在线编译运行)
+    // online code complie web site(www.ideone.com)
+    // regular聽expression standard(www.cplusplus.com/reference/regex/ECMAScript/)
     if(num < 2 || num > 10 || !std::regex_match (str, std::regex("\"[[:alnum:]]{1,50}\"(,\"[[:alnum:]]{1,50}\"){0,49}"))){
         cout<<-1<<","<<-1<<endl;
         return 0;
